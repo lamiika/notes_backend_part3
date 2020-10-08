45f7a77f8d455dfd71e21c4afcb43e13fc3e357f
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-08 22:53:08 +0300

    Add files

---
+++ build/precache-manifest.4eae6c74365e21bb648abf94fe28aec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4898b48e5855dcc03b9d48f9032b871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f9e9497f15de6ceb4b2",
+    "url": "/static/css/main.23202dd9.chunk.css"
+  },
+  {
+    "revision": "4bae58adcee35358e065",
+    "url": "/static/js/2.5bf2cba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5bf2cba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9e9497f15de6ceb4b2",
+    "url": "/static/js/main.3dc11928.chunk.js"
+  },
+  {
+    "revision": "fcfa87c657e9ad05204f",
+    "url": "/static/js/runtime-main.ef781745.js"
+  }
+]);
